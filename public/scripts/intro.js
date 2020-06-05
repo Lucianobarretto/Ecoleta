638aef2500e02df0fb1d0f0b663c5257bd88f582
Luciano Barreto <contato.lucianobarreto@gmail.com>
2020-06-05 13:29:04 -0300

    Refatoração: criação da pasta 'public', movimentação e alteração de arquivos para manipular no Back-end

---
+++ public/scripts/intro.js
@@ -0,0 +1,77 @@
+// coments (comentários usa em JS //)
+
+// document.write("Hello")
+
+// VARIÁVEIS -  tipos de dados
+// // var myvar = "He" (também posso usar no lugar de 'var' o 'let'; var é = nome,
+//                       e let é = contador. A principal diferença entre var e let
+//                       é que let só será acessível nos blocos de código em que
+//                       foi declarada. Os blocos de código são normalmente determinados
+//                       por chaves {}. "PARA MAIS INSTRUÇÕES LER O ARQUIVO PPT SALVO
+//                                       NA PASTA LUCIANO/DOCUMENTO/CURSOS/DIGITAL HOUSE")
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// STRING - é um conjunto de caracteres alfanuméricos
+// posso usa "", ou '', ou ``
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também`
+
+// NUMBER - só números
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// BOOLEAN - true ou false (são os Booleanos)
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// OBJETO - possuem propriedades e funcionalidades
+// const pessoa = {
+//    altura: "1.80m",
+//    idade: 24,
+//    solteiro: true,
+//    correr(){
+//      document.write("executar uma grande lógica de correr")
+//    }
+// }
+
+// pessoa.correr()
+
+// ARRAY - vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+// FUNÇÕES - funcionalidades, atalhos, reuso de código
+
+// // registrar função
+// function sayMyName(name) {
+//    document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Luciano")
+
+// CONDICIONAIS - condições (se, ou, ... entre outros)
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//    document.write("Reprovado")
+// } else {
+//    document.write("Aprovado")
+// }
+
+// LOOP - repetições
+
+// for (i = 0, i < 10, i++) {
+//    document.write("<p>${i}</p>")
+// }
